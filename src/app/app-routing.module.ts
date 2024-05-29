cbc8b1aeb983700b458d4cf8f1138b3ede6d4bde
Pablo <pablostadnik@hotmail.com>
2024-05-29 12:26:52 -0300

    Fotografia preparada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes }  from '@angular/router';
+import { HomePageComponent }  from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent }  from './shared/pages/about-page/about-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+{
+  path:'',
+  component: HomePageComponent
+},
+
+{
+  path:'about',
+  component: AboutPageComponent
+},
+
+{
+  path:'**',
+  redirectTo: 'h'
+},
+{
+  path:'contact',
+  component: ContactPageComponent
+},
+{
+  path:'countries',
+  loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule)
+}
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[
+    RouterModule.forRoot( routes ),
+  ],
+  exports: [
+    RouterModule,
+
+  ]
+
+
 })
 export class AppRoutingModule { }
